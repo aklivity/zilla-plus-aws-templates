ef20542bceb51ea20c9d93aea10e33c055b2c1ea
bmaidics <b.maidics@gmail.com>
2025-05-21 11:38:37 +0200

    Update confluent-cloud/cdk/lib/WebStreamingStack.ts
    Co-authored-by: John Fallows <john.r.fallows@gmail.com>

--- confluent-cloud/cdk/lib/WebStreamingStack.ts
+++ confluent-cloud/cdk/lib/WebStreamingStack.ts
@@ -110,9 +110,8 @@ export class WebStreamingStack extends cdk.Stack {
       context.cloudwatch?.logs?.group !== undefined ||
       context.cloudwatch?.metrics?.namespace !== undefined;
 
-    context.version ??= "latest";
-
     // apply context defaults
+    context.version ??= "latest";
     context.capacity ??= props?.freeTrial ? 1 : 2;
     context.instanceType ??= 'c6i.xlarge';
 
